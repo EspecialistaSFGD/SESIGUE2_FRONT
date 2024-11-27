633d7dd341bcb8f98d433ccf7ed56189e96bdd63
Julio Leonardo <julioleonardoconsultor@gmail.com>
2024-11-27 15:27:07 -0500

    Mejoras panel

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    api: 'http://52.168.137.104:200/testback/api',
-    client: 'http://52.168.137.104:200/testback',
+    api: 'http://52.168.137.104:300/testback/api',
+    client: 'http://52.168.137.104:300/testback',
     topoJsonUrl: 'assets/data/json',
 };
