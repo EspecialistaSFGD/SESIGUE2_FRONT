5c2da31c8194d67e5b64e50ddc4f054b17c1b789
marcos Barrera <mabarreraweb@gmail.com>
2025-03-11 18:10:53 -0500

    metas form added

--- src/app/pages/asistencias-tecnicas/metas/metas-detalles/metas-detalles.component.ts
+++ src/app/pages/asistencias-tecnicas/metas/metas-detalles/metas-detalles.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MetaToDetails } from '@core/interfaces/meta.interface';
 import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
 import { NZ_MODAL_DATA } from 'ng-zorro-antd/modal';
@@ -21,8 +21,15 @@ export class MetasDetallesComponent {
 
   loadingData: boolean = false
   metas: MetasResp[] = []
+  showForm:  boolean = false
 
   readonly meta: MetaToDetails = inject(NZ_MODAL_DATA);
+  private fb = inject(FormBuilder)
+
+  public formMeta: FormGroup = this.fb.group({
+      fecha: ['', Validators.required],
+      meta: ['', Validators.required],
+  })
 
   ngOnInit(): void {
     this.generateDate()
@@ -31,6 +38,7 @@ export class MetasDetallesComponent {
   generateDate(){
     const metas:number[] =  [32,15, 20, 25, 32, 30]
     const fechas: string[] = ['03/01/2025', '02/01/2025', '01/01/2025', '12/01/2024', '11/01/2024', '10/01/202']
+    // const fechas: string[] = ['02/01/2025', '01/01/2025', '12/01/2024', '11/01/2024', '10/01/202']
     let i = 0
     for(let fecha of fechas){
       const metaDate = new Date(fecha);
@@ -42,5 +50,11 @@ export class MetasDetallesComponent {
   enabledEdit(fecha: Date){
     const currentDate = new Date();
     return !(currentDate.getMonth() === fecha.getMonth() && currentDate.getFullYear() === fecha.getFullYear());
+  }
+
+  enabledNewMeta(){
+    const lastDate = this.metas[0].fecha
+    // this.enabledEdit(lastDate)
+    return this.enabledEdit(lastDate)
   }
 }
