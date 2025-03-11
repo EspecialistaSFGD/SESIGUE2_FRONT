9cb340b3c2d7d483259f8b96d0100e8722730217
marcos Barrera <mabarreraweb@gmail.com>
2025-03-11 15:43:59 -0500

    user list added to metas

---
+++ src/app/core/services/usuarios.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { HelpersService } from './helpers.service';
+import { Observable } from 'rxjs';
+import { Pagination, UsuariosResponses } from '@core/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  private urlUsuario: string = `${environment.api}/Usuario`
+    private http = inject(HttpClient)
+    private helpersServices = inject(HelpersService);
+  
+    listarUsuario(pagination: Pagination): Observable<UsuariosResponses> {
+      let params = this.helpersServices.setParams(pagination)
+        // .append("sectorId", sector)
+        // .append("entidadId", entidades)
+      // if (entidades.length > 0) {
+      //     // clasificacion.forEach((clas: SelectModel) => {
+      //     //     params = params.append('clasificacionId[]', `${clas.value}`);
+      //     // });
+      //     for(let entidad of entidades){
+      //       params = params.append('entidad[]', `${clas.value}`);
+      //     }
+      // }
+        // .append("entidadId", entidad)
+        console.log(params);
+        
+      const headers = this.helpersServices.getAutorizationToken()
+      return this.http.get<UsuariosResponses>(`${this.urlUsuario}/ListarUsuarios`, { headers, params })
+    }
+}
