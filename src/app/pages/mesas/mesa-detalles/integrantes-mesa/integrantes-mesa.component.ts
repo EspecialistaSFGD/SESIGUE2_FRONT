829e3be322afb40841491c8dde9c331b7998fe25
marcos Barrera <mabarreraweb@gmail.com>
2025-06-05 18:32:32 -0500

    form modal integrante added

--- src/app/pages/mesas/mesa-detalles/integrantes-mesa/integrantes-mesa.component.ts
+++ src/app/pages/mesas/mesa-detalles/integrantes-mesa/integrantes-mesa.component.ts
@@ -3,6 +3,8 @@ import { Component, inject, Input, signal } from '@angular/core';
 import { MesaResponse, MesaUbigeoResponse, Pagination } from '@core/interfaces';
 import { MesaUbigeosService } from '@core/services';
 import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
+import { NzModalService } from 'ng-zorro-antd/modal';
+import { FormularioIntegranteMesaComponent } from './formulario-integrante-mesa/formulario-integrante-mesa.component';
 
 @Component({
   selector: 'app-integrantes-mesa',
@@ -15,6 +17,16 @@ export class IntegrantesMesaComponent {
   @Input() mesaId!: number
   @Input() esSector:boolean = true
 
+  integrantes = signal<MesaUbigeoResponse[]>([])
+  modal = inject(NzModalService);
+
+  integrante: MesaUbigeoResponse = {
+    mesaId: '',
+    entidadId: '',
+    alcaldeAsistenteId: '',
+    esSector: this.esSector
+  }
+
   loading: boolean = false
   pagination: Pagination = {
     columnSort: 'fechaRegistro',
@@ -23,8 +35,6 @@ export class IntegrantesMesaComponent {
     currentPage: 1,
     total: 0
   }
-
-  integrantes = signal<MesaUbigeoResponse[]>([])
 
   private integrantesService = inject(MesaUbigeosService)
 
@@ -42,5 +52,40 @@ export class IntegrantesMesaComponent {
         this.integrantes.set(resp.data)
         this.pagination.total = resp.info!.total
       })
+  }
+
+  nuevoIntegrante(){
+    this.formularioIntegrante()
+  }
+
+  actualizarIntegrante(integrante: MesaUbigeoResponse){
+    this.integrante = integrante
+    this.formularioIntegrante(false)
+  }
+
+  formularioIntegrante(create: boolean = true){
+    const title = create ? 'AGREGAR' : 'ACTUALIZAR'
+    this.modal.create<FormularioIntegranteMesaComponent>({
+      nzTitle: `${title} INTEGRANTE`,
+      // nzWidth: '75%',
+      nzContent: FormularioIntegranteMesaComponent,
+      nzData: {
+        create: create,
+        integrante: this.integrante,
+      },
+      nzFooter: [
+        {
+          label: 'Cancelar',
+          type: 'default',
+          onClick: () => this.modal.closeAll(),
+        },
+        {
+          label: 'Actualizar Mesa',
+          type: 'primary',
+          onClick: (componentResponse) => {
+          }
+        }
+      ]
+    })
   }
 }
