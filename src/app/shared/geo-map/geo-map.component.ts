296237f1325f5eabc0e2d26a82a02550646f52f5
marcos Barrera <mabarreraweb@gmail.com>
2025-06-26 12:35:30 -0500

    componente geo map started

---
+++ src/app/shared/geo-map/geo-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geo-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './geo-map.component.html',
+  styles: ``
+})
+export class GeoMapComponent {
+
+}
