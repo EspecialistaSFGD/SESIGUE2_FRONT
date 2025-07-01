c59a5de6fc56066edb7ba3604f5ee1bdd28144db
marcos Barrera <mabarreraweb@gmail.com>
2025-07-01 16:26:45 -0500

    footer table added

--- src/app/pages/intervenciones/intervencionPanel/intervencion-panel-estado/intervencion-panel-estado.component.ts
+++ src/app/pages/intervenciones/intervencionPanel/intervencion-panel-estado/intervencion-panel-estado.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input } from '@angular/core';
 import { InterfacePanelResult } from '@core/interfaces/intervencion.interface';
 import { PipesModule } from '@core/pipes/pipes.module';
 import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
@@ -13,6 +13,26 @@ import { TableCardComponent } from '@shared/table-card/table-card.component';
   templateUrl: './intervencion-panel-estado.component.html',
   styles: ``
 })
-export class IntervencionPanelEstadoComponent {
+export class IntervencionPanelEstadoComponent implements AfterViewInit {
   @Input() intervencionEstados: InterfacePanelResult[] = []
+  totales: InterfacePanelResult = {
+    nombre: 'Total', cantIntervenciones: 0, costoActualizado: 0.0, devAcumulado: 0.0, pim: 0.0, devengado: 0.0, avance: 0, inversionActual: 0
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.generarTotales()
+    }, 100)
+  }
+
+  generarTotales(){
+    this.intervencionEstados.find( item => {
+      this.totales.cantIntervenciones += item.cantIntervenciones;
+      this.totales.costoActualizado += item.costoActualizado
+      this.totales.pim += item.pim
+      this.totales.devAcumulado = item.devAcumulado
+      this.totales.devengado = item.devengado
+      this.totales.inversionActual = item.inversionActual      
+    })    
+  }
 }
