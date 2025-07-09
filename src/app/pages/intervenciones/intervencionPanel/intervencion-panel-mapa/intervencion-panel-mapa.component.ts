06bade570db285ff018658c3340d6c2ed7199309
marcos Barrera <mabarreraweb@gmail.com>
2025-07-09 12:57:25 -0500

    set mapa updated

--- src/app/pages/intervenciones/intervencionPanel/intervencion-panel-mapa/intervencion-panel-mapa.component.ts
+++ src/app/pages/intervenciones/intervencionPanel/intervencion-panel-mapa/intervencion-panel-mapa.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, Input, SimpleChanges } from '@angular/core';
 import { departamentosTopoJSON } from '@core/helpers';
-import { Pagination, UbigeoTopoJson } from '@core/interfaces';
+import { GeoTopoJson, Pagination, UbigeoTopoJson } from '@core/interfaces';
 import { InterfacePanelResult } from '@core/interfaces/intervencion.interface';
 import { CardComponent } from '@shared/card/card.component';
 import { GeoMapComponent } from "../../../../shared/geo-map/geo-map.component";
@@ -18,19 +18,13 @@ export class IntervencionPanelMapaComponent {
   @Input() intervencionUbigeo: InterfacePanelResult[] = []
   @Input() pagination!: Pagination
   dataTopoJson: UbigeoTopoJson[] = departamentosTopoJSON()
+  geoTopoJson: GeoTopoJson = { geo: 'departamentos', ubigeo: 'departamentos' }
 
   
   private entidadService = inject(EntidadesService)
 
   ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.pagination);
-    
-    console.log(this.intervencionUbigeo);
-    
-  }
-
-  ngOnInit(): void {
-    setTimeout(() => this.setIntervencionUbigeo(), 100);
+    this.setIntervencionUbigeo()
   }
   
   setIntervencionUbigeo(){
@@ -40,13 +34,25 @@ export class IntervencionPanelMapaComponent {
         data.porcentaje = `${intervencion.avance.toFixed(1)} %` 
       }      
     }
+    console.log(this.pagination);
+    
+    if( this.pagination.entidadUbigeoId && this.pagination.nivelUbigeo ){
+      const entidadId = this.pagination.entidadUbigeoId
+      const nivelUbigeo = Number(this.pagination.nivelUbigeo)
+      this.obtenerEntidadPorId(entidadId, nivelUbigeo)
+    }
   }
 
-  obtenerEntidadPorId(entidadId: string, nivelUbigeo: string ){
+  obtenerEntidadPorId(entidadId: string, nivelUbigeo: number ){
     this.entidadService.getEntidadPorId(entidadId)
       .subscribe( resp => {
         const entidad = resp.data[0]
-        const ubigeo =  entidad.ubigeo.slice(0,2)
+        let ubigeo =  entidad.ubigeo
+        if(nivelUbigeo == 1){
+          ubigeo = ubigeo.slice(0,2)
+        }
+        const geo = nivelUbigeo == 1 ? 'provincias' : 'distritos'
+        this.geoTopoJson = { geo, ubigeo }        
       })
   }
 }
