2a61825f3def0d6823ebc0f9a5f92e795898692a
marcos Barrera <mabarreraweb@gmail.com>
2025-07-22 18:46:54 -0500

    login responsive

--- src/app/libs/shared/layout/footer/footer.component.ts
+++ src/app/libs/shared/layout/footer/footer.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { SocialMedia } from '@core/interfaces';
+import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [
-    NzIconModule,
-  ],
+  imports: [ NgZorroModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.less'
+  styles: []
 })
 export class FooterComponent {
-
+  followUs: SocialMedia[] = [
+    { name: 'Facebook', icon: 'facebook', url: 'https://www.facebook.com/descentralizacionperu' },
+    { name: 'X', icon: 'twitter', url: 'https://x.com/SdPeru' },
+    { name: 'Youtube', icon: 'youtube', url: 'https://www.youtube.com/channel/UCJ9HmmpdeadVknJ8XRRZ8GQ' },
+    { name: 'Linkedin', icon: 'linkedin', url: 'https://pe.linkedin.com/in/secretar%C3%ADa-de-descentralizaci%C3%B3n-pcm' }
+  ]
 }
--- src/app/libs/shared/layout/super-header/super-header.component.ts
+++ src/app/libs/shared/layout/super-header/super-header.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './super-header.component.html',
-  styleUrl: './super-header.component.less'
+  styles: []
 })
 export class SuperHeaderComponent {
 
