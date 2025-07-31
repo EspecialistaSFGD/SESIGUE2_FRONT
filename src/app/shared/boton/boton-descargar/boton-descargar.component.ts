ee13b34b786975921f09f2a7b14d968a07ea9893
marcos Barrera <mabarreraweb@gmail.com>
2025-07-31 11:44:08 -0500

    response donwload file alert

--- src/app/shared/boton/boton-descargar/boton-descargar.component.ts
+++ src/app/shared/boton/boton-descargar/boton-descargar.component.ts
@@ -3,12 +3,15 @@ import { Component, inject, Input } from '@angular/core';
 import { generateBase64ToArrayBuffer } from '@core/helpers';
 import { DescargarService } from '@core/services';
 import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
+import { PrimeNgModule } from '@libs/prime-ng/prime-ng.module';
 import saveAs from 'file-saver';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-boton-descargar',
   standalone: true,
-  imports: [CommonModule, NgZorroModule],
+  imports: [CommonModule, NgZorroModule, PrimeNgModule],
+  providers: [MessageService],
   templateUrl: './boton-descargar.component.html',
   styles: ``
 })
@@ -17,17 +20,25 @@ export class BotonDescargarComponent {
   @Input() rutaArchivo:string = ''
   @Input() active: boolean = true
   @Input() titulo: string = 'Descargar'
+  @Input() preload: boolean = false
+
+  loading: boolean = false
 
   private descargarServices = inject(DescargarService)
+  private messageService = inject(MessageService)
 
   descargarPdf(archivo: string){
+    this.loading = true
       this.descargarServices.descargarPdf(archivo)
-        .subscribe((resp) => {        
+        .subscribe((resp) => {       
           if (resp.success == true) {
             var binary_string = generateBase64ToArrayBuffer(resp.data.binario);
             var blob = new Blob([binary_string], { type: `application/${resp.data.tipo}` });
             saveAs(blob, resp.data.nombre);
+          } else {
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: resp!.message });
           }
+          this.loading = false
         })
     }
 }
