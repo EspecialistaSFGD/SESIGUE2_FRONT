24ed92b0b4fba33b39d5ae144dbfaabea4c1251b
marcos Barrera <mabarreraweb@gmail.com>
2025-07-31 12:55:37 -0500

    detail transference resources started

---
+++ src/app/pages/transferencias-recursos/indice-transferencia-recurso/indice-transferencia-recurso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-indice-transferencia-recurso',
+  standalone: true,
+  imports: [],
+  templateUrl: './indice-transferencia-recurso.component.html',
+  styles: ``
+})
+export class IndiceTransferenciaRecursoComponent {
+
+}
--- src/app/pages/transferencias-recursos/transferencias-recursos.routes.ts
+++ src/app/pages/transferencias-recursos/transferencias-recursos.routes.ts
@@ -2,6 +2,6 @@ import { Routes } from '@angular/router';
 
 export const TRANSFERENCIAS_RECURSOS_ROUTES: Routes = [
   { path: '', loadComponent: () => import('./transferencias-recursos.component') },
-  // { path: ':id', loadComponent: () => import('./mesa-detalles/mesa-detalles.component') },
+  { path: ':id', loadComponent: () => import('./detalle-transferencia-recurso/detalle-transferencia-recurso.component') },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
