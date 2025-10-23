fa8281c355efa7290ae7f4b9c9f3cc33f6cfa145
Marcos Barrera <mabarreraweb@gmail.com>
2025-10-23 02:39:38 -0500

    panel entidad mapa added

--- src/app/pages/panel-entidades/mapa-panel-entidades/mapa-panel-entidades.component.ts
+++ src/app/pages/panel-entidades/mapa-panel-entidades/mapa-panel-entidades.component.ts
@@ -1,13 +1,40 @@
-import { Component, Input } from '@angular/core';
-import { EntidadPanelResponse } from '@core/interfaces';
+import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { departamentosTopoJSON } from '@core/helpers';
+import { EntidadPanelResponse, GeoTopoJson, UbigeoTopoJson } from '@core/interfaces';
+import { CardComponent } from '@shared/card/card.component';
+import { GeoMapComponent } from '@shared/geo-map/geo-map.component';
 
 @Component({
   selector: 'app-mapa-panel-entidades',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent, GeoMapComponent],
   templateUrl: './mapa-panel-entidades.component.html',
   styles: ``
 })
 export class MapaPanelEntidadesComponent {
   @Input() panelUbigeos: EntidadPanelResponse[] = [];
+
+  dataTopoJson: UbigeoTopoJson[] = departamentosTopoJSON()
+  geoTopoJson: GeoTopoJson = { geo: 'departamentos', ubigeo: 'departamentos' }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setIntervencionUbigeo()
+  }
+
+  ngOnDestroy(): void {
+    this.setIntervencionUbigeo()
+  }
+
+  setIntervencionUbigeo(){
+    const newData: UbigeoTopoJson[] = [ ...this.dataTopoJson ]
+      for(let data of newData){
+      const ubigeos = this.panelUbigeos.find(item => item.nombre == data.nombre)
+      if(ubigeos){
+        data.porcentaje = `${ubigeos.porcentaje!.toFixed(1)} %` 
+      }      
+    }
+
+    this.dataTopoJson = newData
+  }
 }
