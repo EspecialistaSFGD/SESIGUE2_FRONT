5d0a5db953f078749be9464592ba0ae2eaface11
Marcos Barrera <mabarreraweb@gmail.com>
2025-10-23 02:20:32 -0500

    card panel entidad started

--- src/app/pages/panel-entidades/ubigeo-panel-entidades/ubigeo-panel-entidades.component.ts
+++ src/app/pages/panel-entidades/ubigeo-panel-entidades/ubigeo-panel-entidades.component.ts
@@ -1,13 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { themeProgressBarPercente } from '@core/helpers';
 import { EntidadPanelResponse } from '@core/interfaces';
+import { PipesModule } from '@core/pipes/pipes.module';
+import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
+import { CardComponent } from '@shared/card/card.component';
 
 @Component({
   selector: 'app-ubigeo-panel-entidades',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent, NgZorroModule, PipesModule],
   templateUrl: './ubigeo-panel-entidades.component.html',
   styles: ``
 })
 export class UbigeoPanelEntidadesComponent {
   @Input() panelUbigeos: EntidadPanelResponse[] = [];
+  totales!: EntidadPanelResponse
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    this.totales = {nombre: 'Total', numero: 0, porcentaje: 0.0}
+    this.generarTotales()
+  }
+
+  colorBarraProgreso(porcentaje: number): string {
+    return themeProgressBarPercente(porcentaje)
+  }
+
+  generarTotales(){
+    this.panelUbigeos.find( item => {
+      this.totales.numero! += item.numero!;
+      this.totales.porcentaje! += item.porcentaje!;
+    })
+    this.totales.porcentaje! = this.totales.porcentaje! > 0 ? this.totales.porcentaje! / this.totales.numero! * 100 : 0
+  }
 }
