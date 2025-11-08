b2058bdc7729c2239ab7238938ce1be041dfa05f
Marcos Barrera <mabarreraweb@gmail.com>
2025-11-08 03:15:11 -0500

    clean perfil added

--- src/app/pages/perfiles/perfil-detalles/perfil-detalles.component.ts
+++ src/app/pages/perfiles/perfil-detalles/perfil-detalles.component.ts
@@ -1,8 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { PerfilResponse } from '@core/interfaces';
-import { PerfilesService } from '@core/services';
+import { Component } from '@angular/core';
 import { NgZorroModule } from '@libs/ng-zorro/ng-zorro.module';
 import { PerfilDetalleComponent } from './perfil-detalle/perfil-detalle.component';
 
@@ -14,37 +11,4 @@ import { PerfilDetalleComponent } from './perfil-detalle/perfil-detalle.componen
   styles: ``
 })
 export default class PerfilDetallesComponent {
-  perfilId:number = 0
-  perfil: PerfilResponse = {} as PerfilResponse
-
-  private router = inject(Router)
-  private route = inject(ActivatedRoute)
-  private perfilService = inject(PerfilesService)
-
-  ngOnInit(): void {
-    this.verificarPerfil()
-  }
-
-  verificarPerfil(){
-    const perfilId = this.route.snapshot.params['id'];
-    const perfilIdNumber = Number(perfilId);
-    if (isNaN(perfilIdNumber)) {
-      this.router.navigate(['perfiles']);
-      return;
-    }
-
-    this.perfilId = perfilIdNumber
-    this.obtenerPerfilService()
-  }
-  
-  obtenerPerfilService(){
-    this.perfilService.obtenerPerfil(this.perfilId.toString())
-      .subscribe( resp => {
-        resp.success ? this.perfil = resp.data : this.router.navigate(['perfiles'])
-      })
-  }
-
-  onBack(){
-    this.router.navigate(['perfiles'])
-  }
 }
--- src/app/pages/perfiles/perfiles.routes.ts
+++ src/app/pages/perfiles/perfiles.routes.ts
@@ -2,7 +2,5 @@ import { Routes } from '@angular/router';
 
 export const PERFILES_ROUTES: Routes = [
   { path: '', loadComponent: () => import('./perfiles.component') },
-  { path: ':id', loadComponent: () => import('./perfil-detalles/perfil-detalles.component') },
-  { path: ':id/accesos/:accesoId', loadComponent: () => import('./perfil-detalles/accesos-perfil/acceso-detalles-perfil/acceso-detalles-perfil.component') },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
